bdec5c76b14e00d8746c47dab071e6126dbe961e
ayush76671kumar@gmail.com <ayush76671kumar@gmail.com>
2026-07-23 11:23:38 +0530

    Added resend email logic

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,67 +1,55 @@
+import { sendWelcomeEmail } from '../emails/emailHandlers.js';
 import { genrateToken } from '../lib/utils.js';
 import User from '../models/User.js';
 import bcrypt from 'bcryptjs';
+import "dotenv/config";
 
 export const signup = async (req, res) => {
-
   // Extract user details from the request body
   const { fullName, email, password } = req.body;
-
   try {
-
     // Check if all required fields are provided
     if (!fullName || !email || !password) {
       return res.status(400).json({
         message: "All fields are required"
       });
     }
-
     // Validate password length
     if (password.length < 6) {
       return res.status(400).json({
         message: "Password must be at least 6 characters long",
       });
     }
-
     // Regular expression to validate email format
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-
     // Check if email format is valid
     if (!emailRegex.test(email)) {
       return res.status(400).json({
         message: "Invalid email format",
       });
     }
-
     // Check whether the email is already registered
     const user = await User.findOne({ email });
-
     if (user) {
       return res.status(400).json({
         message: "Email already exists",
       });
     }
-
     // Generate a salt and hash the user's password before storing it
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
-
     // Create a new user document with the hashed password
     const newUser = new User({
       fullName,
       email,
       password: hashedPassword
     });
-
     // If user object is created successfully
     if (newUser) {
-
-      // Generate JWT token and store it in a cookie
-      genrateToken(newUser._id, res);
-
       // Save the user in MongoDB
-      await newUser.save();
-
+      const savedUser = await newUser.save();
+      // Generate JWT token and store it in a cookie
+      genrateToken(savedUser._id, res);
       // Send user details (excluding password) in the response
       res.status(201).json({
         _id: newUser._id,
@@ -69,20 +57,21 @@ export const signup = async (req, res) => {
         email: newUser.email,
         profilePic: newUser.profilePic
       });
-
+      //Sending email
+      try {
+        await sendWelcomeEmail(savedUser.email, savedUser.fullName, process.env.CLIENT_URL);
+      } catch (error) {
+        console.error("Failed to send welcome email:", error);
+      }
     } else {
-
       // Handle unexpected user creation failure
       res.status(400).json({
         message: "Invalid user data"
       });
     }
-
   } catch (error) {
-
     // Log the error and send a generic server error response
     console.log("Error in signUP ", error);
-
     res.status(500).json({
       message: "Internal server error"
     });
