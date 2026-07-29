a20d43313b1c449dcb1a099e0cb064a9c6e6bfcc
ayush76671kumar@gmail.com <ayush76671kumar@gmail.com>
2026-07-29 15:29:19 +0530

    Added profile-header&logout

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,7 +12,7 @@ const app=express();
 
 app.use(cookieParser());
 app.use(cors({origin:ENV.CLIENT_URL, credentials:true}));
-app.use(express.json());
+app.use(express.json({limit:"5mb"}));
 
 app.use('/api/auth',authRoutes);
 app.use('/api/messages',messageRoutes);
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -57,6 +57,28 @@ export const useAuthStore = create((set) => ({
             set({ isLogingin: false });
         }
     },
+      logout: async () => {
+    try {
+      await axiosInstance.post("/auth/logout");
+      set({ authUser: null });
+      toast.success("Logged out successfully");
+    //   get().disconnectSocket();
+    } catch (error) {
+      toast.error("Error logging out");
+      console.log("Logout error:", error);
+    }
+  },
+
+  updateProfile : async (data) =>{
+    try {
+        const res = await axiosInstance.put("/auth/update-profile",data);
+        set({authUser:res.data});
+        toast.success("Profile updated Successfully");
+    } catch (error) {
+        console.log("Error in update profile:-", error );
+        toast.error(error.response.data.message);
+    }
+  },
 
 
 }))
